eeaff6f659c985233d928502ca567f74e67a5007
JoBenEtuk <JoBenEtuk@users.noreply.github.com>
2021-04-15 13:42:49 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Welcome to the site with the best corny dad jokes in the world</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Welcome To Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
